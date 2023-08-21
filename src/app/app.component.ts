3d1caf70dcfba30223a9c850a4cdfae38704a629
Daniel <dacatieb@gmail.com>
2023-08-21 13:25:21 +0200

    feat(#1): Configuración inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CONFIGURATION } from 'src/global/configuration';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tiendaVideojuegos';
+  nombre = CONFIGURATION.appName;
 }
---
+++ src/global/configuration.ts
@@ -0,0 +1,4 @@
+export const CONFIGURATION = {
+    appName: "GamingHub",
+    description: "Tienda online de videojuegos. Venta para varias plataformas."
+}
